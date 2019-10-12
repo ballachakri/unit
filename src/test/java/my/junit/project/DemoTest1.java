f6d0d26aa8f8be5f4f13f719f4f12f238515d515
chakravarthy <ballachakri@yahoo.com>
2019-10-12 16:32:05 +0100

    unit for pipeline

---
+++ src/test/java/my/junit/project/DemoTest1.java
@@ -0,0 +1,25 @@
+package my.junit.project;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DemoTest1 {
+
+    @Before
+    public void demoTest1Before() {
+        System.out.println("This a before method in demoTest1Before method...................");
+    }
+
+    @Test
+    public void demoTest1Test() {
+        System.out.println("UNIT Test Demo Test 1 test method executed.......................... ");
+        System.out.println("TEST ONE Successfully Passed");
+    }
+
+    @After
+    public void demoTest1After() {
+        System.out.println("This a before method in demoTest1Before method...................");
+    }
+
+}
